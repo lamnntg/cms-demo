3d643b5aeb48fad38860a34c6144ff5a4c8c7f30
lam-nguyen <lamnguyen.hust@gmail.com>
2023-11-26 21:11:26 +0700

    fix upload mutilple

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -42,12 +42,26 @@ class HomeController extends Controller
             if (isset($params['upload'])) {
                 $url = Cloudinary::upload($request->file('upload')->getRealPath())->getSecurePath();
 
-                return response()->json(['fileName' => $request->file('upload')->getClientOriginalName(), 'uploaded'=> 1, 'url' => $url]);
+                return response()->json(['fileName' => $request->file('upload')->getClientOriginalName(), 'uploaded' => 1, 'url' => $url]);
+            }
+
+            if (isset($params['uploads'])) {
+                $uploads = [];
+                foreach ($params['uploads'] as $upload) {
+                    $url = Cloudinary::upload($request->file('upload')->getRealPath())->getSecurePath();
+                    $uploads[] = [
+                        'fileName' => $request->file('upload')->getClientOriginalName(),
+                        'uploaded' => 1,
+                        'url' => $url
+                    ];
+                }
+
+                return response()->json($uploads);
             }
         } catch (\Throwable $th) {
             //throw $th;
         }
 
-        return response()->json(['fileName' => '', 'uploaded'=> 1, 'url' => '']);
+        return response()->json(['fileName' => '', 'uploaded' => 1, 'url' => '']);
     }
 }
