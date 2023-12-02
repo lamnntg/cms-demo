707cb0df46664d88d60dfbab361a0ab3241f7b8d
lam-nguyen <lamnguyen.hust@gmail.com>
2023-12-02 18:16:11 +0700

    Fix

--- resources/views/edit-product.blade.php
+++ resources/views/edit-product.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
 <div id="app">
     <h1 class="h3 mb-4 text-gray-800">{{ __('Chỉnh sửa sản phẩm') }}</h1>
-    <create-product is-edit :product={{$product}} :categories="{{json_encode($categories)}}"></create-product>
+    <create-product is-edit :product="{{($product)}}" :categories="{{json_encode($categories)}}"></create-product>
 </div>
 @endsection
 
