3925e7a00cbfe89d636ba776d9d9518346b87fb9
Nguyen Tung Lam - KV Web <lam.nt4@kiotviet.com>
2023-12-19 17:04:17 +0700

    Fix

--- app/Http/Middleware/FirebaseAuthenticate.php
+++ app/Http/Middleware/FirebaseAuthenticate.php
@@ -31,9 +31,15 @@ class FirebaseAuthenticate
         }
 
         try {
-            $checkRevoked = true;
-            $result = Firebase::auth()->verifyIdToken($accessToken, $checkRevoked)->claims()->all();
-            $firebaseUser = FirebaseUser::where('uid', $result['user_id'])->first();
+            // check and get uid by access token
+            $userId = $this->verifyAndGetUidManual($accessToken);
+            if (!$userId) {
+                $checkRevoked = true;
+                $result = Firebase::auth()->verifyIdToken($accessToken, $checkRevoked)->claims()->all();
+                $userId = $result['user_id'];
+            }
+
+            $firebaseUser = FirebaseUser::where('uid', $userId)->first();
             if (!$firebaseUser) {
                 return response()->json(
                     [
@@ -56,5 +62,30 @@ class FirebaseAuthenticate
         }
 
         return $next($request);
+    }
+
+    /**
+     * verifyAndGetUidManual function
+     *
+     * @param string $accessToken
+     * @return mixed
+     */
+    private function verifyAndGetUidManual($accessToken) {
+        $parseToken = Firebase::auth()->parseToken($accessToken);
+
+        $tokenInfo = $parseToken->claims();
+        $iat = $tokenInfo->get('iat');
+        $exp = $tokenInfo->get('exp');
+        $userId = $tokenInfo->get('user_id');
+        if (now()->getTimestamp() > $exp->getTimestamp()) {
+            return null;
+        }
+
+        // check if token created > 10 minutes ago -> check by SDK
+        if ((now()->getTimestamp() - $iat->getTimestamp()) > 600) {
+            return null;
+        }
+
+        return $userId;
     }
 }
