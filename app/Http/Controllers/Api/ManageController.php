6f2eca7f994a67c638a8fde7d58520d58f145553
Nguyen Tung Lam - KV Web <lam.nt4@kiotviet.com>
2023-12-19 14:20:08 +0700

    Fix

--- app/Http/Controllers/Api/ManageController.php
+++ app/Http/Controllers/Api/ManageController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\HouseArticle;
 use App\Models\MarketArticle;
-use App\Models\News;
 use App\Models\ServiceArticle;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ManageController extends ApiController
@@ -41,7 +41,110 @@ class ManageController extends ApiController
         } catch (\Throwable $th) {
             return $this->response($th, Response::HTTP_BAD_REQUEST);
         }
+    }
 
-        return $this->response('Object not found', Response::HTTP_OK);
+    /**
+     * get House Article
+     * @param $request
+     *
+     * @return JsonResponse
+     */
+    public function getHouseArticles(Request $request)
+    {
+        $user = request()->user();
+
+        $request->validate([
+            'page' => 'nullable|integer',
+            'per_page' => 'nullable|integer',
+            'type' => 'required|in:1,2',
+            'sort_fields' => 'nullable|string|in:bedrooms,wcs,price',
+            'sort_order' => 'nullable|string|in:desc,asc',
+            'from' => 'nullable|string'
+        ]);
+        $params = $request->all();
+        $paginate = [
+            'page' => $params['page'] ?? 1,
+            'per_page' => $params['per_page'] ?? 8
+        ];
+
+        $filter = [
+            'from' => $params['from'] ?? null,
+            'type' => $params['type'],
+            'sort_fields' => $params['sort_fields'] ?? null,
+            'sort_order' => $params['sort_order'] ?? 'ASC',
+        ];
+
+        list($statusCode, $data) = $this->articleService->getHouseArticles($filter, $paginate, $user->id);
+
+        return $this->response($data, $statusCode);
+    }
+
+    /**
+     * getServiceArticles function
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getServiceArticles(Request $request) {
+        $user = request()->user();
+
+        $request->validate([
+            'page' => 'nullable|integer',
+            'per_page' => 'nullable|integer',
+            'sort_fields' => 'nullable|string|in:bedrooms,wcs,price',
+            'sort_order' => 'nullable|string|in:desc,asc',
+            'from' => 'nullable|string'
+        ]);
+
+        $params = $request->all();
+        $paginate = [
+            'page' => $params['page'] ?? 1,
+            'per_page' => $params['per_page'] ?? 8
+        ];
+
+        $filter = [
+            'from' => $params['from'] ?? null,
+            'sort_fields' => $params['sort_fields'] ?? null,
+            'sort_order' => $params['sort_order'] ?? 'ASC',
+        ];
+
+        list($statusCode, $data) = $this->articleService->getServiceArticles($filter, $paginate, $user->id);
+
+        return $this->response($data, $statusCode);
+    }
+
+
+     /**
+     * getMarketArticles function
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getMarketArticles(Request $request) {
+        $user = request()->user();
+
+        $request->validate([
+            'page' => 'nullable|integer',
+            'per_page' => 'nullable|integer',
+            'sort_fields' => 'nullable|string|in:bedrooms,wcs,price',
+            'sort_order' => 'nullable|string|in:desc,asc',
+            'from' => 'nullable|string'
+        ]);
+
+        $params = $request->all();
+        $paginate = [
+            'page' => $params['page'] ?? 1,
+            'per_page' => $params['per_page'] ?? 8
+        ];
+
+        $filter = [
+            'from' => $params['from'] ?? null,
+            'sort_fields' => $params['sort_fields'] ?? null,
+            'sort_order' => $params['sort_order'] ?? 'ASC',
+        ];
+
+        list($statusCode, $data) = $this->articleService->getMarketArticles($filter, $paginate, $user->id);
+
+        return $this->response($data, $statusCode);
     }
 }
