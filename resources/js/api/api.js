3b1a5a7d4e1e82dd4ef28cb4744a1d3ba2e664da
lam-nguyen <lamnguyen.hust@gmail.com>
2023-12-25 22:56:57 +0700

    Fix

--- resources/js/api/api.js
+++ resources/js/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.APP_URL + '/api/v1',
+  baseURL: 'https://manager.prismstudio.vn' + '/api/v1',
   headers: {
     'Content-Type': 'application/json',
     timeout: 1000
